bac7175f0150f4e4e8fe349417bbc6b95e2e0697
유시준 <simba0502@naver.com>
2021-04-12 22:51:02 +0900

    dao service 수정

--- src/everyssafy/model/service/EveryssafyServiceImpl.java
+++ src/everyssafy/model/service/EveryssafyServiceImpl.java
@@ -1,11 +1,7 @@
 package everyssafy.model.service;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 import everyssafy.model.UserDto;
-import util.DBUtil;
+import everyssafy.model.dao.EveryssafyDaoImpl;
 
 public class EveryssafyServiceImpl implements EveryssafyService {
 	private static EveryssafyService everyssafyService;
@@ -17,43 +13,13 @@ public class EveryssafyServiceImpl implements EveryssafyService {
 	@Override
 	public UserDto login(String userId, String userPwd) {
 		// TODO Auto-generated method stub
-		UserDto ret=null;
-		return ret;
+		return EveryssafyDaoImpl.getEveryssafyDao().login(userId, userPwd);
 	}
 
 	@Override
 	public void register(String userId, String userPwd, String userName, String userNickName) {
 		// TODO Auto-generated method stub
-		UserDto userDto=null;
-		Connection conn=null;
-		PreparedStatement pstmt=null;
-		try {
-			conn=DBUtil.getConnect();
-			String sql="insert into user(userId,userPwd,userName,userNickName) values(?,?,?,?)";
-			pstmt=conn.prepareStatement(sql);
-			pstmt.setString(1, userId);
-			pstmt.setString(2, userPwd);
-			pstmt.setString(3, userName);
-			pstmt.setString(4, userNickName);
-			int result=pstmt.executeUpdate();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}finally {
-			if(pstmt!=null) {
-				try {
-					pstmt.close();
-				}catch(SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			if(conn!=null) {
-				try {
-					conn.close();
-				}catch(SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+		EveryssafyDaoImpl.getEveryssafyDao().register(userId, userPwd, userName, userNickName);
 	}
 
 }
