543b6c9ac5ecc04b13fd131db47edc9ad8b76d9f
유시준 <simba0502@naver.com>
2021-04-12 22:42:19 +0900

    merge용

--- src/everyssafy/model/service/EveryssafyServiceImpl.java
+++ src/everyssafy/model/service/EveryssafyServiceImpl.java
@@ -1,6 +1,11 @@
 package everyssafy.model.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import everyssafy.model.UserDto;
+import util.DBUtil;
 
 public class EveryssafyServiceImpl implements EveryssafyService {
 	private static EveryssafyService everyssafyService;
@@ -19,7 +24,36 @@ public class EveryssafyServiceImpl implements EveryssafyService {
 	@Override
 	public void register(String userId, String userPwd, String userName, String userNickName) {
 		// TODO Auto-generated method stub
-
+		UserDto userDto=null;
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		try {
+			conn=DBUtil.getConnect();
+			String sql="insert into user(userId,userPwd,userName,userNickName) values(?,?,?,?)";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			pstmt.setString(2, userPwd);
+			pstmt.setString(3, userName);
+			pstmt.setString(4, userNickName);
+			int result=pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn!=null) {
+				try {
+					conn.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
