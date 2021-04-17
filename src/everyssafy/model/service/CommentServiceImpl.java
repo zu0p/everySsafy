e14f58f54f5ebb7a1aeb0955590c4586cddbea07
KyeongY1214 <ykh1214@naver.com>
2021-04-17 13:35:27 +0900

    Comment Dao Dto Service 껍데기

---
+++ src/everyssafy/model/service/CommentServiceImpl.java
@@ -0,0 +1,35 @@
+package everyssafy.model.service;
+
+import java.util.List;
+
+import everyssafy.model.CommentDto;
+
+public class CommentServiceImpl implements CommentService
+{
+
+	@Override
+	public void registerComment(CommentDto commentDto) {
+		
+	}
+
+	@Override
+	public CommentDto getComment(int commentDto) {
+		return null;
+	}
+
+	@Override
+	public void modifyComment(CommentDto commentDto) {
+		
+	}
+
+	@Override
+	public void deleteComment(int commentDto) {
+		
+	}
+
+	@Override
+	public List<CommentDto> getCommentList(int articleId) {
+		return null;
+	}
+
+}
